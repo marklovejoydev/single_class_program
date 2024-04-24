70af7d62e706e770020a599664dc7b62cefef0ce
marklovejoy <marklovejoydev@gmail.com>
2024-04-24 14:41:47 +0100

    added completed grammar_stats with tests set up todo_list ready for next step

--- lib/exercises/grammer_stats.py
+++ lib/exercises/grammer_stats.py
@@ -1,17 +1,20 @@
 class GrammarStats:
     def __init__(self):
-        pass
+        self.total_count = 0
+        self.correct_count = 0
 
     def check(self, text):
-        # Parameters:
-        #   text: string
-        # Returns:
-        #   bool: true if the text begins with a capital letter and ends with a
-        #         sentence-ending punctuation mark, false otherwise
-        pass
+        if text[0].isupper() and text[-1] in".?!":
+            self.total_count += 1
+            self.correct_count += 1
+            return True
+        else:
+            self.total_count += 1
+            return False
 
     def percentage_good(self):
-        # Returns:
-        #   int: the percentage of texts checked so far that passed the check
-        #        defined in the `check` method. The number 55 represents 55%.
-        pass
+        if self.total_count == 0:
+            return "No checks performed yet"
+        else:
+            percentage = int((self.correct_count / self.total_count) * 100)
+            return f"{percentage}%"
--- tests/test_grammer_stats.py
+++ tests/test_grammer_stats.py
@@ -1,1 +1,20 @@
 from lib.exercises.grammer_stats import GrammarStats
+
+
+def test_uppercase_and_punctuation_is_present_at_start_and_end():
+    grammar = GrammarStats()
+    assert grammar.check("Hello!") == True
+    assert grammar.check("Hello") == False
+    assert grammar.check("hello!") == False
+    
+def test_percentage_of_checks_that_passed_against_total():
+    grammar = GrammarStats()
+    for _ in range(10):
+        grammar.check("hello")
+        grammar.check("Hello!")
+    assert grammar.percentage_good() == "50%"
+    
+def test_percentage_when_nothing_has_been_checked():
+    grammar = GrammarStats()
+    assert grammar.percentage_good() == "No checks performed yet"
+    
